36c7858ef27eab2c8e97c030b12fc823455db107
William Doody <wdoody@yahoo.com>
2018-04-04 20:27:54 -0600

    added helicopter movement variables

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <title>Phaser Template</title>
-    <link rel="stylesheet" href="styles/main.css">
-    <link rel="manifest" href="manifest.json">
-    <script>
-        if (navigator.serviceWorker) {
-            navigator.serviceWorker.register('service-worker.js')
-                .then(function () {
-                console.log('service worker installed');
-            })
-            .catch(function (err) {
-                console.log('Error', err);
-        });
-}
-	</script>
-	
-</head>
-<body>
-    <script src="scripts/phaser.min.js"></script>
-    <script src="scripts/game.js"></script>
-</body>
-</html>
+$(document).ready(function () {
+//Play button
+    $("#Play").click(function () {
+//   load new game.html page?  or change class to reveal canvas potentially? --> $("p").slideToggle();
+
+    });
+});
+
+var canvas = document.getElementById('canvas'); 
+var ctx = canvas.getContext('2d');
+
+
+//create a buttload of variables 
+
+
+
+//do something with canvas
+
+//do controls for helicopter
+
+//chopper movement on canvas
+let heli
+let heliX
+let heliY
+let heliWidth = 50
+let heliHeight = 25
+ctx.drawImage(heli, heliX, heliY, heliWidth, heliHeight);
+
+
+
+//figure out how to create the obstacles --- prob something with math.random
+
+//colisions???
+
+// how do i make this a game that you can win? Do i need to?
