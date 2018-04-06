cb2825a74d5fbf4d710c36f9be6a20f8757ded3c
William Doody <wdoody@yahoo.com>
2018-04-06 10:20:28 -0600

    rect that moves with direction keys and 2 non-moving rect

--- main.js
+++ main.js
@@ -1,40 +1,67 @@
-// $(document).ready(function () {
-//Play button
- //   $("#Play").click(function () {
-  //  });
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+
+$(document).ready(function () {
+    //Play button
+    //   $("#Play").click(function () {
+     //  });
 
   //create a buttload of variables 
-
+    
     var heliX
     var heliY
-    var heliWidth = 150
-    var heliHeight = 125
+    var heliWidth = 20
+    var heliHeight = 25
+  
+    var bombArr;
+    var bombHeight = 60;
+    var bombWidth = 30;
+    var bombColor = "red";
+
+    var backgroundHeight = 350;
+    var backgroundWidth = 700;
+    var background = new Image();
+    background.src = "pic2.jpg"
     var heli = new Image();
     heli.src = "heli.png";
 
 
-$(document).ready(function () {
-    $("#start").click(function () {
-        $("splashScreen").fadeOut();  //   load new game.html page?  or change class to reveal canvas potentially? --> $("p").slideToggle();
-    });
 
+    // $("#start").click(function () {
+    //     $("splashScreen").fadeOut();  //   load new game.html page?  or change class to reveal canvas potentially? --> $("p").slideToggle();
+    // });
 
+//   window.onload = function () {
 
-    var canvas = document.getElementById('screen');
+    var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
+        ctx.drawImage(background, 0, 0, backgroundWidth, backgroundHeight);
+        ctx.drawImage(heli, 10, 30, heliWidth, heliHeight);
+
+    //   function setup() {
+    //       bombArr = new Array();
 
 
-    window.onload = function () {
-  
-        function setup() {
-            ctx.fillStyle = 'rgb(200, 0, 0)'; // in place of heli at the moment
-            ctx.fillRect(10, 40, 30, 50);
+//            };
+        // function obstacle() {
+        //     newBomb = {}
+        //     newBomb.x = 50    //canvas.width;
+        //     newBomb.y = 50   //Math.floor(Math.random() * (canvas.height - brickHeight))
+        //     BombArr.push(newBomb);
+//        }
 
-        // heli.src = "heli.png";
+
+  //  }
+    
+
+      // heli.src = "heli.png";
         // heliY = 100;
         // heliY = 175;
         // ctx.drawImage(heli, heliX, heliY, heliWidth, heliHeight);
 
+  
 
 
 
@@ -44,15 +71,6 @@ $(document).ready(function () {
 
 
 
-
-
-
-    };
-
-
-        
-
-    }
 
 
 
@@ -64,34 +82,39 @@ $(document).ready(function () {
 
 
 
-
+    $("#bombs").click(function () {
+    addBrick();
+    });
 
 //figure out how to create the obstacles --- prob something with math.random
 
-
-
-
 //colisions???
 
+ //   if (object1.x < object2.x + object2.width && object1.x + object1.width > object2.x &&
+   //     object1.y < object2.y + object2.height && object1.y + object1.height > object2.y) {
+        // The objects are touching
+ //   }
+
+
+ 
 // how do i make this a game that you can win? Do i need to?
 
 
-
-// ----------heli movement -below-----------
-    var stage = document.getElementById('screen'), 
-        ctx = stage.getContext('2d'), 
+// ----------heli movement ----below-----------------------
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
         x = 10, 
         y = 10, 
-        wid = 20, 
-        hei = 20; 
+        width = 20, 
+        height = 20; 
 
     //Draw square (heli) 		
-    function drawRect(x, y, wid, hei) {
+    function drawRect(x, y, width, height) {
         ctx.fillStyle = '#666'; 
-        ctx.fillRect(x, y, wid, hei); 
+        ctx.fillRect(x, y, width, height); 
     }
 
-    drawRect(x, y, wid, hei); 
+    drawRect(x, y, width, height); 
 
     //move rectangle (heli) using arrow keys
     window.onkeydown = function (event) {
@@ -114,9 +137,29 @@ $(document).ready(function () {
         ctx.clearRect(0, 0, 500, 500);
 
 //redraw heli
-        drawRect(x, y, wid, hei);
+        drawRect(x, y, width, height);
     };
-// ----------heli movement -----above-----------
+// ----------heli movement -----above------------------------------
+
+    var bomb = document.getElementById("canvas");
+    var ctx = bomb.getContext("2d");
+
+    // Red rectangle
+    ctx.beginPath();
+    ctx.lineWidth = "6";
+    ctx.strokeStyle = "red";
+    ctx.rect(97, 100, 20, 150);
+    ctx.stroke();
+    drawRect(x, y, width, height);
+
+    // Green rectangle
+    ctx.beginPath();
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "green";
+    ctx.rect(130, 0, 20, 60);
+    ctx.stroke();
+
+
 
 
 //dont delete this closing tag
