4905f599d3e5090022846b685e1c781df9cf5418
William Doody <wdoody@yahoo.com>
2018-04-09 22:10:01 -0600

    landing page framework

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,148 +1,119 @@
 
-function setup() {
-    createCanvas(500, 500);
+
+var myGamePiece;
+var myObstacles = [];
+var myScore;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece.gravity = 0.05;
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myGameArea.start();
 }
 
-function draw() {
-    background(250);
-}
-
-var player = { x: 150, y: 250, size: 50 };
-var coins = [];
-var gravity = 0;
-var score = 0;
-var goUp = false;
-var crashed = false;
-var gap = {height: 350, y: 250};
-var walls = [];
-var wallTimer = 0;
-
-var draw = function() {
-    noStroke();
-    background(0, 0, 0);
-    drawPlayer();
-    drawWalls();
-    drawScore();
-
-    if (crashed === false) {
-        movePlayer();
-        doCoin();
-        moveWalls();
-    }   else { 
-        youLoseScreen();
-        }
-};
-var drawWalls = function () {
-    for (var wall of walls) {
-    fill("green");
-    rect(wall.x, wall.y, wall.w, wall.h)
-}
-};
-var moveWalls = function () {
-    for (var wall of walls) {
-        wall.x -= 3;
-    
-     if (wall.x < player.x && wall.x + wall.w > player.x) {
-         if (player.y - player.size/2 < wall.y + wall.h && 
-        player.y + player.size/2 > wall.y) {
-        crashed = true;
-        }
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
-    if (wallTimer <= 0) {
-        wallTimer = 16;
-        gap.y += 25 * floor(random(2.66) - 1);
 
-        var newWall = { x: 500, y: 0, w: 50, h: gap.y - gap.height/2 };
-        walls.push(newWall);
-        var newWall = { x: 500, y: gap.y + 100, w: 50, h: gap.y - gap.height/2 };
-        walls.push(newWall);
-    }
-    wallTimer -= 1;
-};
-
-
-    if (gap.y < 150) {
-        gap.y = 150
-    }
-    if (gap.y > 350) {
-        gap.y = 350;
-    };
-
-var doCoin = function() {
-    var filteredCoins = coins.filter((coin) => {return coin.x > 0 && !coin.collected});
-    coins = filteredCoins;
-    if (random(0,100)< 3) {
-        var newCoin = {x:600, y: random(0, 500), size: 20, collected: false};
-        coins.push(newCoin);
-    }
-    for (var coin of coins) {
-
-        fill(255,255,0);
-        ellipse(coin.x, coin.y, coin.size, coin.size);
-
-        coin.x -= 3;
-        var playerRadius = player.size/2;
-        var coinRadius = coin.size/2;
-        var touchDistance = playerRadius + coinRadius;
-
-        if (dist(player.x, player.y, coin.x, coin.y) < touchDistance) {
-            coin.collected = true;
-            score += 1;
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.update = function () {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-};
-
-
-var drawPlayer = function () {
-    fill(0, 0, 255);
-    ellipse(player.x, player.y, player.size, player.size);
-}
-var movePlayer = function () {
-    if (goUp) {
-    gravity -= 0.4;
-    } else {
-        gravity += 0.4;
+    this.newPos = function () {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
     }
-    if (gravity > 8) {
-        gravity = 8;
-    }
-    if (gravity < -6) {
-        gravity = -6;
-    }
-    player.y += gravity;
-
-if (player.y > 500 || player.y < 0) {
-    crashed = true;
-    }
-};
-var mousePressed = function() {
-    if (mouseButton === LEFT) {
-        goUp = true;
-        if (crashed) {
-            crashed = false;
-            player.y = 250;
-            gravity = 0;
-            score = 0;
-            coins = [];
-            walls = [];
+    this.hitBottom = function () {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
         }
     }
-};
-var mouseReleased = function() {
-    if (mouseButton === LEFT) {
-        goUp = false;
+    this.crashWith = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
     }
-};
-var youLoseScreen = function() {
-    fill(255); 
-    textSize(24);
-    text("Game Over", 200, 200);
-    text("click to restart", 180, 350);
-};
-var drawScore = function() {
-    fill(255, 255, 0);
-    textSize(24);
-    text(score, 50, 450);
-};
+}
+
+function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            return;
+        }
+    }
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        x = myGameArea.canvas.width;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+        myObstacles.push(new component(10, height, "green", x, 0));
+        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+    myScore.text = "SCORE: " + myGameArea.frameNo;
+    myScore.update();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
+    return false;
+}
+
+function accelerate(n) {
+    myGamePiece.gravity = n;
+}
 
